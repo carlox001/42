48959ee104c1dee5a8f629f2e917e9642ea9152c
Carlox <147998586+carlox001@users.noreply.github.com>
2024-08-21 17:41:35 +0200

    Add files via upload

--- So_long/so_long.c
+++ So_long/so_long.c
@@ -6,19 +6,36 @@
 /*   By: czerini <czerini@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 17:41:51 by czerini           #+#    #+#             */
-/*   Updated: 2024/08/11 19:15:14 by czerini          ###   ########.fr       */
+/*   Updated: 2024/08/21 15:47:24 by czerini          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-typedef struct s_data
+int	keypress(int keycode, t_data *data)
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
-} t_data;
+	static int	count_move = -1;
 
-int	on_destroy(t_data *data)
+	if (keycode == 65307)
+	{
+		close_window(data);
+	}
+	else if (keycode == 'w' && data->y > 0)
+		data->y -= 10;
+	else if (keycode == 's' && data->y + data->tex_hei < 400)
+		data->y += 10;
+	else if (keycode == 'a' && data->x > 0)
+		data->x -= 10;
+	else if (keycode == 'd' && data->x + data->tex_wid < 600)
+		data->x += 10;
+	mlx_clear_window(data->mlx_ptr, data->win_ptr);
+	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->tex_img, data->x, data->y);
+	count_move++;
+	printf("Number of moves: %d\n", count_move);
+	return (0);
+}
+
+int	close_window(t_data *data)
 {
 	mlx_destroy_window(data->mlx_ptr, data->win_ptr);
 	mlx_destroy_display(data->mlx_ptr);
@@ -26,25 +43,32 @@ int	on_destroy(t_data *data)
 	exit(0);
 	return(0);
 }
-int	on_keypress(int keysym, t_data *data)
-{
-	(void)data;
-	printf("Pressed key: %d\\n", keysym);
-	return (0);
-}
 
 int	main(void)
 {
 	t_data	data;
 
+	
 	data.mlx_ptr = mlx_init();
 	if (!data.mlx_ptr)
 		return (1);
-	data.win_ptr = mlx_new_window(data.mlx_ptr, 600, 400, "ciao");
+	
+	data.win_ptr = mlx_new_window(data.mlx_ptr, 600, 400, "Moving Character");
 	if (!data.win_ptr)
 		return (free(data.mlx_ptr), 1);
-	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &on_keypress, &data);
-	mlx_hook(data.win_ptr, DestroyNotify, StructureNotifyMask, &on_destroy, &data);
+	
+	data.img = mlx_new_image(data.mlx_ptr, 500, 500);
+	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length, &data.endian);
+	data.tex_img = mlx_xpm_file_to_image(data.mlx_ptr, "solo_soldier.xpm", &data.tex_wid, &data.tex_hei);
+	if (!data.tex_img)
+		return (free(data.mlx_ptr), free(data.win_ptr), 1);
+	data.x = (600 - data.tex_wid) / 2;
+	data.y = (400 - data.tex_hei) / 2;
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.tex_img, 250, 250);
+	
+	mlx_hook(data.win_ptr, 17, 0, &close_window, &data);
+	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &keypress, &data);
+	mlx_hook(data.win_ptr, DestroyNotify, StructureNotifyMask, &close_window, &data);
 	mlx_loop(data.mlx_ptr);
 	return (0);
 }
--- So_long/so_long.h
+++ So_long/so_long.h
@@ -6,7 +6,7 @@
 /*   By: czerini <czerini@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/10 14:58:02 by czerini           #+#    #+#             */
-/*   Updated: 2024/08/10 16:43:08 by czerini          ###   ########.fr       */
+/*   Updated: 2024/08/21 14:44:57 by czerini          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,30 @@
 # include "minilibx-linux/mlx_int.h"
 # include <X11/X.h>
 # include <X11/keysym.h>
+# include <X11/Xlib.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <math.h>
+
 
 typedef struct s_data
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+	void	*tex_img;
+	int		tex_wid;
+	int		tex_hei;
+	int		x;
+	int		y;
 } t_data;
 
-int	on_destroy(t_data *data);
-int	on_keypress(int keysym, t_data *data);
+int	close_window(t_data *data);
+int	keypress(int keycode, t_data *data);
 int	main(void);
 
 #endif
