803fdad2b07ced20c29b90029ebae30dbdc559e6
VMUbuntu <carlotta.zerini01@gmail.com>
2024-08-23 13:56:44 +0200

    so_long test

---
+++ so_long_test/so_long.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: czerini <czerini@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/10 14:58:02 by czerini           #+#    #+#             */
+/*   Updated: 2024/08/22 16:41:46 by czerini          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+# include "minilibx-linux/mlx.h"
+# include "minilibx-linux/mlx_int.h"
+# include <X11/X.h>
+# include <X11/keysym.h>
+# include <X11/Xlib.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <math.h>
+
+#endif
